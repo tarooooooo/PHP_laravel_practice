7002e7a5a02fdfe3dd1fe723bc52c948bbff5828
tarooooooo <ryutaromaeda83@gmail.com>
2021-11-07 13:44:50 +0900

    routeのグループ化

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -18,17 +18,34 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-// 一覧画面表示
-Route::get('/tasks', [App\Http\Controllers\TaskController::class, 'index']);
-// 追加画面を表示するための設定
-Route::get('/tasks/create', [App\Http\Controllers \TaskController::class, 'create']);
-// 編集画面を表示するための設定
-Route::get('/tasks/{id}/edit', [App\Http\Controllers \TaskController::class, 'edit']);
-// 削除画面を表示するための設定
-Route::get('/tasks/{id}', [App\Http\Controllers \TaskController::class, 'show']);
-//タスク登録
-Route::post('/tasks', [App\Http\Controllers \TaskController::class, 'store']);
-// タスク更新
-Route::patch('/tasks/{id}', [App\Http\Controllers \TaskController::class, 'update']);
-// タスク削除
-Route::delete('/tasks/{id}', [App\Http\Controllers \TaskController::class, 'destroy']);
+// // 一覧画面表示
+// Route::get('/tasks', [App\Http\Controllers\TaskController::class, 'index']);
+// // 追加画面を表示するための設定
+// Route::get('/tasks/create', [App\Http\Controllers \TaskController::class, 'create']);
+// // 編集画面を表示するための設定
+// Route::get('/tasks/{id}/edit', [App\Http\Controllers \TaskController::class, 'edit']);
+// // 削除画面を表示するための設定
+// Route::get('/tasks/{id}', [App\Http\Controllers \TaskController::class, 'show']);
+// //タスク登録
+// Route::post('/tasks', [App\Http\Controllers \TaskController::class, 'store']);
+// // タスク更新
+// Route::patch('/tasks/{id}', [App\Http\Controllers \TaskController::class, 'update']);
+// // タスク削除
+// Route::delete('/tasks/{id}', [App\Http\Controllers \TaskController::class, 'destroy']);
+
+Route::group(['prefix' => 'tasks', 'as' => 'tasks.'], function(){
+    Route::get('/',
+    [App\Http\Controllers\TaskController::class, 'index']);
+    Route::get('/create',
+    [App\Http\Controllers \TaskController::class, 'create']);
+    Route::post('',
+    [App\Http\Controllers \TaskController::class, 'store']);
+    Route::get('/{id}/edit',
+    [App\Http\Controllers \TaskController::class, 'edit']);
+    Route::patch('/{id}',
+    [App\Http\Controllers \TaskController::class, 'update']);
+    Route::get('/{id}',
+    [App\Http\Controllers \TaskController::class, 'show']);
+    Route::delete('/{id}',
+    [App\Http\Controllers \TaskController::class, 'destroy']);
+});
