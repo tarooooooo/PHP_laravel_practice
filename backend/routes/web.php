281a91e983c928c978980ea4c6c7b447673b6fd0
tarooooooo <ryutaromaeda83@gmail.com>
2021-11-08 19:05:35 +0900

    routepractice (パラメータ)

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -36,7 +36,24 @@ $html = <<<EOF
 </html>
 EOF;
 
-Route::get('hello', function() use ($html){
+Route::get('hello/{msg?}', function($msg='no message.'){
+    $html = <<<EOF
+        <html>
+            <head>
+                <title>Hello</title>
+                <style>
+                    body {font-size: 16pt; color: #999; }
+                    h1 {  font-size:100pt; text-align:right; color:#eee; margin:-40px 0px -50px 0px; }
+                </style>
+            </head>
+            <body>
+                <h1>Hello</h1>
+                <p>$msg</p>
+                <p>これは、サンプルで作ったページです</p>
+            </body>
+        </html>
+    EOF;
+
     return $html;
 });
 // // 一覧画面表示
