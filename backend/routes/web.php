dbb6707888412cc85761fe2d6b795043fa92468b
tarooooooo <ryutaromaeda83@gmail.com>
2021-11-08 18:55:32 +0900

    routeoractice(ヒアドキュメントを利用)

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -19,8 +19,25 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('hello', function(){
-    return '<html><body><h1>Hello</h1><p>This is sample page.</p></body></html>';
+$html = <<<EOF
+<html>
+    <head>
+        <title>Hello</title>
+        <style>
+            body {font-size: 16pt; color: #999; }
+            h1 {  font-size:100pt; text-align:right; color:#eee; margin:-40px 0px -50px 0px; }
+        </style>
+    </head>
+    <body>
+        <h1>Hello</h1>
+        <p>This is sample page.</p>
+        <p>これは、サンプルで作ったページです</p>
+    </body>
+</html>
+EOF;
+
+Route::get('hello', function() use ($html){
+    return $html;
 });
 // // 一覧画面表示
 // Route::get('/tasks', [App\Http\Controllers\TaskController::class, 'index']);
