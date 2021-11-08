85e0fb8a3c91ee8bf81f6d76b2fb4503d2822a87
tarooooooo <ryutaromaeda83@gmail.com>
2021-11-09 00:49:37 +0900

    複数アクションの利用

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -13,49 +13,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-
-
 Route::get('/', function () {
     return view('welcome');
 });
 
-$html = <<<EOF
-<html>
-    <head>
-        <title>Hello</title>
-        <style>
-            body {font-size: 16pt; color: #999; }
-            h1 {  font-size:100pt; text-align:right; color:#eee; margin:-40px 0px -50px 0px; }
-        </style>
-    </head>
-    <body>
-        <h1>Hello</h1>
-        <p>This is sample page.</p>
-        <p>これは、サンプルで作ったページです</p>
-    </body>
-</html>
-EOF;
+Route::get('hello', 'HelloController@index');
+Route::get('hello/other', 'HelloController@other');
 
-Route::get('hello/{msg?}', function($msg='no message.'){
-    $html = <<<EOF
-        <html>
-            <head>
-                <title>Hello</title>
-                <style>
-                    body {font-size: 16pt; color: #999; }
-                    h1 {  font-size:100pt; text-align:right; color:#eee; margin:-40px 0px -50px 0px; }
-                </style>
-            </head>
-            <body>
-                <h1>Hello</h1>
-                <p>$msg</p>
-                <p>これは、サンプルで作ったページです</p>
-            </body>
-        </html>
-    EOF;
-
-    return $html;
-});
 // // 一覧画面表示
 // Route::get('/tasks', [App\Http\Controllers\TaskController::class, 'index']);
 // // 追加画面を表示するための設定
