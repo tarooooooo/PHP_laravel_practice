1b49e8676b4f3f7e9396ea04b3f641b2c80e6cd5
tarooooooo <ryutaromaeda83@gmail.com>
2021-11-09 22:36:55 +0900

    @eachディレクティブの学習

--- backend/resources/views/hello/index.blade.php
+++ backend/resources/views/hello/index.blade.php
@@ -8,20 +8,13 @@
 @endsection
 
 @section('content')
-   <p>ここが本文のコンテンツです。</p>
-   <p>必要なだけ記述できます。</p>
+  <p>ここが本文のコンテンツです。</p>
 
-   @component('components.message')
-       @slot('msg_title')
-       CAUTION!
-       @endslot
+  <ul>
+    @each('components.item', $data, 'item')
+  </ul>
 
-       @slot('msg_content')
-       これはメッセージの表示です。
-       @endslot
-   @endcomponent
 @endsection
-
 @section('footer')
 copyright 2020 tuyano.
 @endsection
