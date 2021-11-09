ba83f573f412cb6a643c0a53541972d2d3eb4750
tarooooooo <ryutaromaeda83@gmail.com>
2021-11-09 14:16:29 +0900

    @componentの学習

--- backend/resources/views/hello/index.blade.php
+++ backend/resources/views/hello/index.blade.php
@@ -1,27 +1,27 @@
-<html>
-<head>
-   <title>Hello/Index</title>
-   <style>
-   body {font-size:16pt; color:#999; }
-   h1 { font-size:50pt; text-align:right; color:#f6f6f6;
-       margin:-20px 0px -30px 0px; letter-spacing:-4pt; }
-   </style>
-</head>
+@extends('layouts.helloapp')
 
-<body>
-   <h1>Blade/Index</h1>
-   <p>&#064;whileディレクティブの例</p>
-   <ol>
-   @php
-   $counter = 0;
-   @endphp
-   @while ($counter < count($data))
-   <li>{{$data[$counter]}}</li>
-   @php
-   $counter++;
-   @endphp
-   @endwhile
-   </ol>
-</body>
+@section('title', 'Index')
 
-</html>
+@section('menubar')
+   @parent
+   インデックスページ
+@endsection
+
+@section('content')
+   <p>ここが本文のコンテンツです。</p>
+   <p>必要なだけ記述できます。</p>
+
+   @component('components.message')
+       @slot('msg_title')
+       CAUTION!
+       @endslot
+
+       @slot('msg_content')
+       これはメッセージの表示です。
+       @endslot
+   @endcomponent
+@endsection
+
+@section('footer')
+copyright 2020 tuyano.
+@endsection
