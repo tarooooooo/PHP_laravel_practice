fd80f260aaad54ec1ba8c531e5eabbc1421576f0
tarooooooo <ryutaromaeda83@gmail.com>
2021-11-09 23:12:42 +0900

    ビューコンポーザの学習

--- backend/app/Http/Controllers/HelloController.php
+++ backend/app/Http/Controllers/HelloController.php
@@ -10,12 +10,7 @@ class HelloController extends Controller
   
    public function index()
    {
-       $data = [
-           ['name'=>'山田たろう', 'mail'=>'taro@yamada'],
-           ['name'=>'田中花子', 'mail'=>'hanako@tanaka'],
-           ['name'=>'鈴木さちこ', 'mail'=>'sachiko@suzuki']
-       ];
-       return view('hello.index', ['data'=>$data]);
+       return view('hello.index', ['message'=>'Hello!']);
    }
 
    public function post(Request $request)
